3736c08c4f3068afd3af9dc7efa6de464d0ea986
aditya <aditya@gmail.com>
2026-08-22 21:40:01 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "adityatfstate001"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "kulcha"
+  location = "Central India"
+}
+
+resource "azurerm_storage_account" "stg" {
+    name = "ramoo21"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+}
+
+resource "azurerm_resource_group" "rg1" {
+  name     = var.rg_names
+  location = var.rg_location
+}
+
+
+
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "rg_names" {
+  type        = string
+  default     = "mangolia"
+  description = "name of resource group"
+}
+
+variable "rg_location" {
+    default = "west us"
+
+}
